67ae3f52b39b00f753079200266449bd442fc0a3
miao1366 <1029081208@qq.com>
2021-08-19 21:26:39 +0800

    2021-08-19

--- c++.cc
+++ c++.cc
@@ -92,3 +92,180 @@ constexpr限定在了编译期常量
     struct is_class
     : public integral_constant<bool, __is_class(_Tp)>
     { };
+
+6. std::is_pod
+当一个数据类型满足了”平凡的定义“和”标准布局“，我们则认为它是一个POD数据，非静态成员都是”平凡的“
+1.平凡的
+2.平凡的
+3.非静态成员“平凡的”
+#include <iostream>
+#include <type_traits>
+struct A {
+    int m;
+};
+struct B {
+    int m1;
+private:
+    int m2;
+};
+struct C {
+    virtual void foo();
+};
+int main() {
+    std::cout << std::boolalpha;
+    std::cout << std::is_pod<A>::value << '\n';
+    std::cout << std::is_pod<B>::value << '\n';
+    std::cout << std::is_pod<C>::value << '\n';
+}
+输出：
+true
+false
+false
+
+7. POD类型
+上一篇：《深入理解C++11》笔记–列表初始化
+本编继续介绍第三章的内容：POD类型，plain old data。Plain代表数据是普通类型，old代表能与C兼容支持memcpy、memset等函数。POD分为两个部分，trivial(平凡的)和(s’tan’dard layout)标准布局的，必须同时满足才是POD类型。
+平凡的类或结构体必须满足以下的条件：
+- 平凡的默认构造函数和析构函数。只要是自己定义了函数，即使实现为空，也不再平凡。所以就是说不能自定义默认构造函数和析构函数。
+- 平凡的拷贝构造函数和移动构造函数。
+- 平凡的赋值构造函数。
+- 不能包含虚函数和虚基类。
+另外，可以用std::is_trivial来判断是不是一个平凡类型，std::is_trivial<T>::value，
+class TrivialA {
+};
+class TrivialB {
+public :
+    int a;
+};
+class TrivialC {
+    TrivialC() {}                   // 有默认构造函数，不平凡
+};
+class TrivialD {
+    TrivialD(const TrivialD& a) {}   // 有赋值构造函数，不平凡
+};
+class TrivialE {
+    TrivialE(TrivalE&& a) {}        // 有移动构造函数，不平凡
+};
+class TrivialF {
+    TrivialF& operator=(const TrivialF& a) {}   // 有赋值构造函数，不平凡
+};
+class TrivialG {
+    virtual void func() = 0;                  // 有虚函数，不平凡
+};
+class TrivialH: virtual public TrivialA {               // 有虚基类，不平凡
+};
+
+int main(int argc, char **argv)
+{
+    std::cout << std::is_trivial<TrivialA>::value << std::endl;     // 1
+    std::cout << std::is_trivial<TrivialB>::value << std::endl;     // 1
+    std::cout << std::is_trivial<TrivialC>::value << std::endl;     // 从这里开始都是0
+    std::cout << std::is_trivial<TrivialD>::value << std::endl;
+    std::cout << std::is_trivial<TrivialE>::value << std::endl;
+    std::cout << std::is_trivial<TrivialF>::value << std::endl;
+    std::cout << std::is_trivial<TrivialG>::value << std::endl;
+    std::cout << std::is_trivial<TrivialH>::value << std::endl;
+
+    return 0;
+}
+标准布局的类或结构体必须满足以下的条件：
+
+所有非静态成员有相同的访问权限。
+在类或结构体继承时满足以下两个条件之一：
+1、派生类中有非静态成员，且只有仅包含静态成员的基类。
+2、基类有非静态成员，而派生类没有非静态成员。
+其实就是派生类和基类中不允许同时出现非静态成员，因为同时有非静态成员就无法进行memcpy
+类中第一个非静态成员的类型与基类不同。
+C++标准允许，在基类没有成员时，派生类第一个成员与基类共享地址。但是当派生类中第一个数据成员类型为基类类型时，有趣的问题就来了。首先，这时派生类的内存布局包括基类部分的内存布局，同时自己又添加了另外一个基类类型的变量，如果编译器优化实现第一个成员和基类部分共享地址，那么就违背了C++标准的另一个要求，同类型的不同对象地址必须不同。
+没有虚函数和虚基类。
+所有非静态成员均符合标准布局，其基类也符合标准布局。
+另外，可以用std::is_standard_layout来判断是不是一个标准布局，std::is_standard_layout<T>::value，例如：
+
+class StdLayoutA {
+};
+class StdLayoutB {
+public :
+    int a;
+    int b;
+};
+class StdLayoutC : public StdLayoutA {
+public:
+    int a;
+    int b;
+    void fun() {}
+};
+class StdLayoutD : public StdLayoutA  {
+public:
+    int a;
+    StdLayoutA sla;
+};
+class StdLayoutE : public StdLayoutA , public StdLayoutC {
+};
+class StdLayoutF {
+public:
+    static int a;
+};
+class StdLayoutG : public StdLayoutF {
+public:
+    int a;
+};
+class StdLayoutH: public StdLayoutA {   // 第一个非静态成员是基类，所以不标准，如果sla的位置和b交换，那么是标准的
+public:
+    StdLayoutA sla;
+    int b;
+};
+class StdLayoutI : public StdLayoutB {   // 基类和派生类都有非静态变量，所以不标准，如果基类或者派生类中只有静态变量，那么是标准的
+public:
+    int a;
+};
+class StdLayoutJ: public StdLayoutI {    // 基类不标准，所以不标准，如果StdLayoutI标准，那么是标准的
+};
+class StdLayoutK{    // 非静态成员权限不同，所以不标准，如果a或b有一个是静态，那么是标准的
+public:
+    int a;
+private:
+    int b;
+};
+
+int main(int argc, char **argv)
+{
+    std::cout << std::is_standard_layout<StdLayoutA>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutB>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutC>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutD>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutE>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutF>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutG>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutH>::value << std::endl;   // 以上都是1，从这里开始都是0
+    std::cout << std::is_standard_layout<StdLayoutI>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutJ>::value << std::endl;
+    std::cout << std::is_standard_layout<StdLayoutK>::value << std::endl;
+
+    return 0;
+}
+
+针对POD类型也有模板类：std::is_pod<T>::value。了解了POD类型的要求，再来解释POD类型的好处：
+
+字节赋值，可以使用memset和memcpy。
+兼容C语言的内存布局
+保证静态初始化安全有效
+
+8.  迭代器
+    输入迭代器input_iterator： 只读，且只能一次读操作，支持操作：++p,p++,!=,==,=*p,p->；
+    输出迭代器output_iterator： 只写，且只能一次写操作，支持操作：++p,p++；
+    正向迭代器forward_iterator： 可多次读写，支持输入输出迭代器的所有操作；
+    双向迭代器bidirectional_iterator： 支持正向迭代器的所有操作，且支持操作：--p,--p；
+    随机访问迭代器random_access_iterator： 除了支持双向迭代器操作外，还支持：p[n],p+n,
+    n+p,p-n,p+=n,p-=n,p1-p2,p1<p2,p1>p2,p1>=p2,p1<=p2；
+
+        /*五中迭代器类型*/  https://blog.csdn.net/qq_34777600/article/details/80427463
+    struct input_iterator_tag {};  
+    struct output_iterator_tag {};  
+    struct forward_iterator_tag : public input_iterator_tag {};  
+    struct bidirectional_iterator_tag : public forward_iterator_tag {};  
+    struct random_access_iterator_tag : public bidirectional_iterator_tag {};  
+
+9.  原来在C++语言中的确规定了空结构体和空类所占内存大小为1，而C语言中空类和空结构体占用的大小是0（在gcc中测试为0，其他编译器不一定）。
+    原来，C++语言标准中规定了这样一个原则：“no object shall have the same address in memory as any other variable”，即任何不同的对象不能拥有相同的内存地址。如果空类对象大小为0，那么此类数组中的各个对象的地址将会一致，明显违反了此原则。
+
+10. 
